bf52fcbeb0bd90d3a560912316e894af7eb994d2
Yuri Golobokov <golobokov@google.com>
2024-05-15 23:07:38 +0000

    load and register c2p provider explicitly

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,17 @@ package org.example;
 
 import com.google.cloud.spanner.Spanner;
 import com.google.cloud.spanner.SpannerOptions;
+import io.grpc.NameResolverProvider;
+import io.grpc.NameResolverRegistry;
 
 public class Main {
   public static void main(String[] args) throws Exception {
+    NameResolverProvider c2pProvider =
+      (NameResolverProvider) Class.forName("io.grpc.googleapis.GoogleCloudToProdNameResolverProvider")
+        .getDeclaredConstructor().newInstance();
+
+    NameResolverRegistry.getDefaultRegistry().register(c2pProvider);
+
     SpannerOptions options = SpannerOptions.newBuilder().build();
     Spanner spanner = options.getService();
   }
